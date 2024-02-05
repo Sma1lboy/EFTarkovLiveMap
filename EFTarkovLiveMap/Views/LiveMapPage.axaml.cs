5f1b027b8c7f7399c1ced43766495c3898aa5386
Sma1lboy <chen.10724@buckeyemail.osu.edu>
2024-02-04 20:35:14 -0600

    feat: adding options for all the map

--- EFTarkovLiveMap/Views/LiveMapPage.axaml.cs
+++ EFTarkovLiveMap/Views/LiveMapPage.axaml.cs
@@ -14,9 +14,10 @@ using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
-using System.Drawing;
-using System.Drawing.Imaging;
+
 using System.IO;
+using EFTarkovLiveMap.Utils;
+using Microsoft.CodeAnalysis.Text;
 
 namespace EFTarkovLiveMap.Views;
 
@@ -30,7 +31,7 @@ public partial class LiveMapPage : UserControl
         InitializeComponent();
         this.DataContext = new MainWindowViewModel();
         var vm = DataContext as MainWindowViewModel;
-        vm.ShortCutKey = Key.F11;
+        vm.ShortCutKey = Key.F9;
 
     }
 
@@ -40,58 +41,12 @@ public partial class LiveMapPage : UserControl
         RecordingText.Text = "Waiting for key press...";
         
     }
-    private string _lastFileName = "";
-
-    public string GetPosition()
-    {
-        var files = Directory.GetFiles(imgPath);
-        if (files.Length == 0)
-        {
-            return _lastFileName;
-        }
-
-        _lastFileName = Path.GetFileName(files[0]);
-        File.Delete(Path.Combine(imgPath, _lastFileName));
-
-        return _lastFileName;
-    }
-
-    private void deleteSomeUseLessDiv(ChromeDriver driver)
-    {
-        var leftPane = driver.FindElement(By.XPath("/html/body/div/div/div/div[2]/div/div/div[2]"));
-        var rightPane = driver.FindElement(By.XPath("/html/body/div/div/div/div[2]/div/div/div[3]"));
-        var header = driver.FindElement(By.XPath("/html/body/div/div/div/header"));
-        var superHeader = driver.FindElement(By.XPath("/html/body/div/div/div/div[1]"));
-        var footer = driver.FindElement(By.XPath("/html/body/div/div/div/div[3]"));
-        // 创建一个 JavaScript 执行器
-        IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-
-        // 移除元素
-        if (leftPane != null) {
-            js.ExecuteScript("arguments[0].remove();", leftPane);
-        }
-
-        if (rightPane != null) {
-            js.ExecuteScript("arguments[0].remove();", rightPane);
-        }
-
-        if (header != null) {
-            js.ExecuteScript("arguments[0].remove();", header);
-        }
-        if (superHeader != null) {
-            js.ExecuteScript("arguments[0].remove();", superHeader);
-        }
-        if (footer != null) {
-            js.ExecuteScript("arguments[0].remove();", footer);
-        }
-
-    }
+    
     private void KeyDownHandler(object? sender, KeyEventArgs e)
     {
         var vm = DataContext as MainWindowViewModel;
         if (IsRecordingNextKey)
         {
-            // DataContext.ShortCutKey = e.Key;
             IsRecordingNextKey = false;
             vm.ShortCutKey = e.Key;
             RecordingText.Text = "Key set to " + e.Key;
@@ -101,51 +56,22 @@ public partial class LiveMapPage : UserControl
             vm.TurnOnOverlay = !vm.TurnOnOverlay;
             if (vm.TurnOnOverlay)
             {
-
-                var options = new ChromeOptions();
-                options.AddArgument("--headless");
-                var driver = new ChromeDriver(options);
-                driver.Navigate().GoToUrl("https://tarkov-market.com/maps/factory");
-                // Assuming there is an input element with id 'inputId'
-                var button = driver.FindElement(By.XPath("/html/body/div/div/div/div[2]/div/div/div[1]/div/button"));
-                Thread.Sleep(1000);
-                button.Click();
-                var input = driver.FindElement(By.XPath("/html/body/div/div/div/div[2]/div/div/div[1]/div/input"));
-                Thread.Sleep(1000);
-                input.SendKeys(GetPosition());
-                var map = driver.FindElement(By.Id("map"));
-                var point = map.Location;
-                var size = map.Size;
+                var a = ComboBox.SelectionBoxItem.ToString();
                 var web = Map.GetInstance();
-
-
-                deleteSomeUseLessDiv(driver);
-                // 截取整个网页
-                Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
-                
-                using (var bmpScreen = new Bitmap(new MemoryStream(screenshot.AsByteArray)))
+                Avalonia.Media.Imaging.Bitmap map = 
+                    MapImageFetcher.FetchMapImage(ComboBox.SelectionBoxItem.ToString());
+                if (map == null)
                 {
-                    
-                    Rectangle cropArea = new Rectangle(0, 0, bmpScreen.Width, bmpScreen.Height - 200);
-                    using (Bitmap croppedImage = bmpScreen.Clone(cropArea, bmpScreen.PixelFormat))
-                    {
-                        using (MemoryStream memory = new MemoryStream())
-                        {
-                            croppedImage.Save(memory, ImageFormat.Png);
-                            memory.Position = 0;
-                            web.sourceImage(new Avalonia.Media.Imaging.Bitmap(memory));
-                        }
-                    }
+                    Console.Write("the name of the map is not correct");
+                    return;
                 }
-                driver.Quit();
-                Console.Write(web);
+                web.sourceImage(map);
                 web.Show();
             }
             else
             {
                 Map.GetInstance().Hide();
             }
-            
         }
     }
 
